7798e6e6eddc0dbbb7f61f6fc1816babb166d1cc
xinruoyusixian <xinruoyusixian@163.com>
2020-02-20 18:26:47 +0800

    更新操作逻辑
    重写研究了一下 小爱的交互逻辑  比较稳定了、根据设备类型可兼容温度计 和开关了

--- xm.py
+++ xm.py
@@ -1,11 +1,33 @@
+
 from robust import MQTTClient
 import utime,ujson,urequests,ntptime
 import network
-from machine import RTC
+from machine import RTC,Pin
+
+
+#gpio 控制的引脚
+#st 引脚的值
+def pin(gpio=2,st=1):
+    if st==1:
+        Pin(gpio,Pin.OUT).value(1)
+    elif st==0:
+        Pin(gpio,Pin.OUT).value(0)
+    elif st==2:
+        return Pin(gpio).value()
+
+
 def wifi(ssd,pwd):
+
+
     #连接WiFi
+
+
     wifi0 = network.WLAN(network.STA_IF)  #创建连接对象 如果让ESP32接入WIFI的话使用STA_IF模式,若以ESP32为热点,则使用AP模式
+
+
     if not wifi0.isconnected(): #判断WIFI连接状态
+
+
         print('connecting to network[正在连接]...')
         wifi0.active(True) #激活WIFI
         wifi0.connect(ssd, pwd) #essid为WIFI名称,password为WIFI密码
@@ -31,23 +53,55 @@ def getInfo(auth):
     ''' 
     return data
 def MI(msg):
+
+
   #2秒内响应否则小爱会说设备不理他
+
+
   #回应小爱
+
+
   #可按需发送 也可分开发送
+
+
   #当小爱同学向设备发起控制, 设备端需要有对应控制处理函数
+
+
   #参考来源：https://github.com/blinker-iot/blinker-library/blob/20125223dc3511ecf08a5afe73de0dadef5519c0/src/Functions/BlinkerMIOT.h 
 
   #看起来一起把消息发给小爱服务器比较妥当
+
+
   '''
+
+
   #数据结构
+
+
   {"pState":"true",#反馈电源状态  [必须]
+
+
   'State':'True', #设备状态  [必须]
+
+
   'mode":'',      #反馈运行模式 [为电灯或温度计时可不加] [DAY	日光,NIGHT	月光,COLOR	彩光,WARMTH	温馨,TV 电视模式,READING  阅读模式,COMPUTER	电脑模式]
-  'num':'1',      # 未知
+
+
+  'num':'0',      # 未知 0 或 ""
+
+
   'temp':'100',   #温度摄氏度[当设备为温度计时]
+
+
   'humi':'0',     #湿度      [当设备为温度计时]
+
+
   'pm25':'10',    #PM2.5浓度 [当设备为温度计时]
+
+
   'pm10':'10',    #PM 10浓度 [当设备为温度计时]
+
+
   'co2':'10'      #co2浓度   [当设备为温度计时]
   }
   '''
@@ -56,19 +110,31 @@ def MI(msg):
   #处理小爱响应  
   try:
     if(msg['get']=="state"):
-      c.publish(pubtopic,playload({'pState':'true', 'State':'True','num':'1','temp':'100','humi':'0', 'pm25':'10','pm10':'10','co2':'10'},toDevice='MIOT_r',deviceType='vAssistant'))
+      c.publish(pubtopic,playload({'State':'True','temp':'100','humi':'0', 'pm25':'10','pm10':'10','co2':'10'},toDevice='MIOT_r',deviceType='vAssistant'))
+      pass
   except:
     pass
   #作为电灯时的操作  
-  try:  
+  
+  try:
     if(msg['set']['pState']==1):
+      if pin(2,2)==0:
+        pass
+      else:
+        pin(2,0)
       print("开启")
     if(msg['set']['pState']==0):
+      if pin(2,2)==1:
+        pass
+      else:
+        pin(2,1)
       print ("关闭")
+    #获取电源状态，操作完毕回复小爱 
+    pin_State= "true" if pin(2,2)==0 else "false"
+    c.publish(pubtopic,playload({"pState":pin_State, 'State':'True',},toDevice='MIOT_r',deviceType='vAssistant'))
   except:
     pass
-  #操作完毕回复小爱  
-  c.publish(pubtopic,playload({"pState":'true', 'State':'True','num':'1','temp':'100','humi':'0', 'pm25':'10','pm10':'10','co2':'10'},toDevice='MIOT_r',deviceType='vAssistant'))
+  print ("已回复小爱")
 #mqtt消息处理  
 def sub_cb(topic, msg):
   print("Mqtt接收<<<<",msg)
@@ -90,17 +156,31 @@ key='60975280bf3e'
 #设置设备类型
 _type="https://iot.diandeng.tech/api/v1/user/device/diy/auth?authKey="+key+"&miType=light&version=1.2.2"
 print ("这个复制到浏览器运行只要运行一次就可以了 /r"+_type)
+
+
 info=  getInfo(key)
+
+
 def playload(msg,toDevice=info['detail']['uuid'],deviceType='OwnApp'):
+
+
     #处理需要发送的数据
+
+
    _data= ujson.dumps({
+
+
    'fromDevice': info['detail']['deviceName'] ,
+
+
    'toDevice':   toDevice,
+
    'data':       msg , 
    'deviceType': deviceType})
    print ("Mqtt发送>>>>",_data)
    return _data
 #mqtt 连接部分
+
 SERVER = "public.iot-as-mqtt.cn-shanghai.aliyuncs.com"
 USER=info['detail']['iotId']
 PWD=info['detail']['iotToken']
