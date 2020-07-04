abbd4bc9f75f8ab159be03bea858120a398e4b77
xinruoyusixian <xinruoyusixian@163.com>
2020-07-05 00:48:09 +0800

    更新断线重连 验证错误重连

--- blinker.py
+++ blinker.py
@@ -1,18 +1,8 @@
 
 
-
-
-
-
-
 import ujson
-from robust import MQTTClient
+from simple import MQTTClient
 from urequests import get 
-
-
-
-
-
 
 class  blinker:
   '''
@@ -22,49 +12,58 @@ class  blinker:
   '''
   def __init__(self,key,cb,devTpye="light"):
     self.devTpye=devTpye
+    self.cb=cb
     self.key=key
+    #self.info=  self.getInfo(key,self.devTpye) 
     self.info= self.input_json_data_from_file("blinker_login_conf.py")
-    SERVER = "public.iot-as-mqtt.cn-shanghai.aliyuncs.com"
-    USER=self.info['detail']['iotId']
-    PWD=self.info['detail']['iotToken']
-    CLIENT_ID =  self.info['detail']['deviceName']
-    self.c=MQTTClient(client_id=CLIENT_ID,server=SERVER,user=USER,password=PWD,keepalive=300)
+    self.SERVER = "public.iot-as-mqtt.cn-shanghai.aliyuncs.com"
+    self.USER=self.info['detail']['iotId']
+    self.PWD=self.info['detail']['iotToken']
+    self.CLIENT_ID =  self.info['detail']['deviceName']
+    self.connect()
+         
+  #获取登录信息
+  def getInfo(self,auth,type_="light"):
+      host = 'https://iot.diandeng.tech'
+      url = '/api/v1/user/device/diy/auth?authKey=' + auth + "&miType="+type_+"&version=0.1.0"
+      data =  get(host + url).text
+      fo = open("blinker_login_conf.py", "w")
+      fo.write(str(data))
+      fo.close()
+      return data
+
+
+  #MQQT 连接    
+  def connect(self):
+    self.c=MQTTClient(client_id=self.CLIENT_ID,server=self.SERVER,user=self.USER,password=self.PWD,keepalive=30)
     self.c.DEBUG = True
-    self.c.set_callback(cb)
+    self.c.set_callback(self.cb)
     self.subtopic="/"+self.info['detail']['productKey']+"/"+self.info['detail']['deviceName']+"/r"
     self.pubtopic=b"/"+self.info['detail']['productKey']+"/"+self.info['detail']['deviceName']+"/s"
-    print("user:",USER,"CLIENT_ID:",CLIENT_ID,self.subtopic,"/r",self.pubtopic)
+    print("user:",self.USER,"CLIENT_ID:",self.CLIENT_ID,self.subtopic,"/r",self.pubtopic)
     if not self.c.connect(clean_session=False):
           print("New session being set .")
           try: 
             self.c.subscribe(self.subtopic)
           except:
-            print("login failed")
+            print("login failed!")
             self.getInfo(self.key,self.devTpye)
             self.__init__(self.key,self.devTpye)
-         
-  #获取登录信息
-  
-  def getInfo(self,auth,type_="light"):
-      host = 'https://iot.diandeng.tech'
-      url = '/api/v1/user/device/diy/auth?authKey=' + auth + "&miType="+type_+"&version=0.1.0"
-      data =  get(host + url).text
-
-      fo = open("blinker_login_conf.py", "w")
-
-
-      fo.write(str(data))
-
-
-      fo.close()
-      return data
-
-
-      
-
-
-      
-
+  #mqtt 信息轮询
+  def check_msg(self):
+      try:
+        self.c.check_msg()
+      except OSError as e:
+        print ("msg check:",e)
+        self.reconnect()
+  #MQQT 重连      
+  def reconnect(self):
+    try:
+      self.c.connect(False)
+      print("reconnected!")
+    except OSError as e:
+        print ("reconnect:",e)
+        self.connect()
   #心跳回复
   def ping(self):  
     self.publish({"state":"online"}) 
@@ -81,7 +80,8 @@ class  blinker:
      'deviceType': deviceType})
      #print ("Mqtt发送>>>>",_data)
      return _data
-  #发布消息   
+     
+  #发布消息
   def publish(self,dict,toDevice="app"):
       if toDevice=="app":
          toDevice=''
@@ -89,9 +89,14 @@ class  blinker:
       if toDevice=="mi":
          toDevice='MIOT_r'
          deviceType='vAssistant'
-      self.c.publish(self.pubtopic,self.playload(dict,toDevice,deviceType))
+      try:   
+        self.c.publish(self.pubtopic,self.playload(dict,toDevice,deviceType))
+      except OSError as e:
+        print ("publish:",e)
+        self.reconnect()
         
-
+        
+  
   def input_json_data_from_file(self,path):
       """
       从文件中获取json数据
@@ -102,91 +107,68 @@ class  blinker:
           with open(path, 'r+') as f:
               try:
                   json_data = ujson.load(f)
-
-
               except Exception as e:
-
-
                   print('json format is error：' + str(e))
-
-
           return json_data
-
-
       except Exception as e:
-
           print("file is not exist")
           self.getInfo(self.key,self.devTpye)
           return  self.input_json_data_from_file("blinker_login_conf.py")
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-            
+
+
+    
 
 if __name__ == "__main__": 
-  from machine import Pin
-  import utime
-  pin_s={}
-  def pin(gpio=2,st=1):
-      global pin_s
-      if st==1:
-          Pin(gpio,Pin.OUT).value(1)
-          pin_s[gpio]=1
-      elif st==0:
-          Pin(gpio,Pin.OUT).value(0)
-          pin_s[gpio]=0
-      elif st==2:
-          try: 
-            return pin_s[gpio]
-          except:
-            pin_s[gpio]=0
-            return 1
-         
-  def cb(topic, msg):
+   import   time ,lib,blinker
 
-        print("Mqtt接收<<<<",msg)
-        #传感器操作
-        mq.publish({"pState":"True","temp":"25.8","humi":"66.8", "pm25":"10","co2":"10"},"mi")
-        #电灯操作
-        msg=eval(str(msg)[2:-1])
-        if msg['fromDevice']=='MIOT':
-          print (msg)
+
+  def cb(topic, msg):
+          print("Mqtt REC<<<<",msg)
+          #传感器操作
+          msg=eval(str(msg)[2:-1])
+          print("Mqtt接收<<<<",msg)
+          #电灯操作
           try:
-            msg['data']['get']
-            #判断电灯状态      
-            if(pin_s[12]==0):
-              print("up_on")
-              #mq.publish({"pState":"True"},"mi")
-            else:
-              print("up_off")
-              #mq.publish({"pState":"False"},"mi")
-          except:
-            pass
-          #设定电灯状态   
-          try:
-            if(msg['data']['set']['pState']=='true'):
-              print("on")
-              pin(12,1)
-              mq.publish({"pState":"true"},"mi")
-            else:
-              print("off")
-              pin(12,0)
-              mq.publish({"pState":"False"},"mi")
+            if msg['data']['btn-sw']=='tap':
+               if lib.pin_s[12]==0:
+                 lib.pin(12,1)
+                 mq.publish({"btn-sw":{"col":"#000000", }})    
+               else:
+                lib.pin(12,0)
+                mq.publish({"btn-sw":{"col":"#FFFFFF", }})
           except:
             pass        
-        mq.ping()
+          if msg['fromDevice']=='MIOT':
 
-  mq=blinker("fbc6da5da621",cb,'sensor')  
+            try:
+              if(msg['data']['set']['pState']=='true'):
+                print("on")
+                lib.pin(12,1)
+
+              else:
+                print("off")
+                lib.pin(12,0)
+
+            except:
+              pass 
+
+          mq.ping()
+          print("exec")
+
+  a="0"
+  mq=blinker.blinker("b14b891ec11b",cb,'light')
+
+
+
+
+  t=1
+  lib.pin(2,1)
+  lib.pin(12,0)
+  mq.ping()
   while 1:
-          utime.sleep(1)
-          mq.c.check_msg()
+      if t%120==0:
+        mq.ping()
+      time.sleep(1)
+      t+=1
+      mq.check_msg()
+      print(t)
